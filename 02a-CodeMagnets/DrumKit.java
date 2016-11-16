0b063ffb23a419408fe4efdeed7d3dca34a61634
Wyatt Phillips <datscigye@gmail.com>
2016-11-16 17:36:10 +0000

    02a-CodeMagnets 1.0 Release

---
+++ 02a-CodeMagnets/DrumKit.java
@@ -0,0 +1,12 @@
+class DrumKit {
+	public boolean topHat = true;
+	public boolean snare = true;
+
+	void playSnare() {
+		System.out.println("bang bang ba-bang");
+	}
+
+	void playTopHat() {
+		System.out.print("ding ding da-ding");
+	}
+}
